3b702a83567693bdd4bd23cd98c1fb30466f3047
dhirajdusane <dhirajdusane@gmail.com>
2023-11-15 05:34:48 +0530

    Fix readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# scripts
+# Scripts
+
+Please swith to 'win' branch to load scripts relevant to windows.
+
+Please swith to 'linux' branch to load scripts relevant to linux.
+
--- keycloak.cmd
+++
@@ -1,2 +0,0 @@
-:: Start Keycloak on a different server port:
-docker run --name keycloak_dev -p 8180:8180 -e KEYCLOAK_ADMIN=admin -e KEYCLOAK_ADMIN_PASSWORD=admin quay.io/keycloak/keycloak:latest start-dev --http-port=8180
